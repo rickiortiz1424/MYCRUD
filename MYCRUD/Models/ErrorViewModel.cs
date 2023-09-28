31d7bf6176af5970366a65ca784871c6903951d6
Ricki Ortiz <rickiortiz1424>
2023-09-27 21:19:42 -0400

    crear Proyecto

---
+++ MYCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MYCRUD/Models/ViewModels/EmpleadoVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MYCRUD.Models.ViewModels
+{
+    public class EmpleadoVM
+    {
+        public Empleado oEmpleado { get; set; }
+        public List<SelectListItem> oListaCargo { get; set; }
+    }
+}
